9997e92c61ea4695a1daf94debd97d31a1f068a2
Arthur Margarian <arthurmargarian1997@gmail.com>
2019-02-15 20:11:52 +0400

    aa

---
+++ FW/app/views/register/login.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Karen
+ * Date: 2/15/2019
+ * Time: 7:15 PM
+ */
---
+++ FW/core/Model.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Karen
+ * Date: 2/15/2019
+ * Time: 6:02 PM
+ */
